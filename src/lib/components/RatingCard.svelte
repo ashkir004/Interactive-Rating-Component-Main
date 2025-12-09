4820bbe0a3a4838eee1ce5e7f2d8ac82ec19dd56
ashkir <55173206+ashkir004@users.noreply.github.com>
2025-12-09 20:29:17 +0000

    Refactor rating to use radio buttons

--- src/lib/components/RatingCard.svelte
+++ src/lib/components/RatingCard.svelte
@@ -1,38 +1,17 @@
 <script>
     import { fade } from 'svelte/transition';
-	import Rating from "./Rating.svelte";
-
-    let ready = $state(false);
-    let submitted = $state(false);
-    let rating = $state(-1);
-
-    $effect(() => ready = true);
-
-    function setRating(ratingNum) {
-        rating = ratingNum;
-    }
-
+	import RatingForm from './RatingForm.svelte';
 </script>
 
 
 
 <article class="card" transition:fade>
-    <div class="star-bg"></div>
     <div class="card-body">
         <h1 class="title">How did we do?</h1>
         <p class="description">Please let us know how we did with your support request.
             All feedback is appreciated to help us improve our offering!</p>
-    
-        <Rating onSelect={(num) => setRating(num)} />
     </div>
-    <form action="/" method="POST">
-        <button disabled={!ready && rating === -1}
-                onclick={() => submitted = true} 
-            type="submit" 
-            name="rating" 
-            value={rating} 
-            class='submit {submitted ? 'submitted' : ''}'>Submit</button>
-    </form>
+    <RatingForm />
 </article>
 
 
@@ -42,16 +21,17 @@
         padding: var(--s-300);
     }
 
-    .star-bg {
-        align-self: first baseline;
+    .card:before {
+        content: '';
         min-width: var(--size-40);
         min-height: var(--size-40);
         border-radius: 50%;
-        background-color: var(--surface-3);
-        background-image: url('/icon-star.svg');
+        background-image: url('/static/icon-star.svg');
         background-repeat: no-repeat;
         background-position: center center;
-        animation: blurIn 3s ease forwards;
+        background-color: var(--surface-3);
+        align-self: flex-start;
+        animation: blurIn 2s ease forwards;
     }
 
     .card-body {
@@ -63,27 +43,6 @@
     .description {
         line-height: var(--line-height-lg);
     }
- 
-    .submit {
-        background-color: var(--primary);
-        color: var(--onprimary);
-        border: none;
-        padding: var(--s-200) 0;
-        text-transform: uppercase;
-        font-family: 'Overpass', sans-serif;
-        font-size: var(--font-xs);
-        font-weight: 700;
-        line-height: var(--line-height-lg);
-        cursor: pointer;
-        border-radius: calc(22.5 / 16 * 1rem);
-        width: 100%;
-        vertical-align: center;
-    }
-
-    .submit.submitted {
-        background-color: var(--secondary);
-        color: var(--onsecondary);
-    }
 
     @media (min-width: 48rem) {
 
@@ -94,10 +53,6 @@
         .star-bg {
             min-width: var(--size-48);
             min-height: var(--size-48);   
-        }
-
-        .submit {
-            padding: var(--s-100) 0;
         }
     }
 
---
+++ src/lib/components/RatingForm.svelte
@@ -0,0 +1,128 @@
+<script>
+    let selected = $state(-1);
+    let submitted = $state(false);
+    $inspect(selected);
+</script>
+
+
+<form action="/" method="POST">
+    <fieldset class="ratings" aria-label="rate us">
+        {#each [1, 2, 3, 4, 5] as num (num)}
+        <label for={num}
+            class="option {selected === num ? 'selected' : ''}">
+            <input
+                type="radio" 
+                id={num} 
+                tabIndex={num}
+                name="rating" 
+                value={num} 
+                bind:group={selected}/>
+            <span>{num}</span>
+        </label>
+        {/each}
+    </fieldset>
+    <button
+        onclick={() => submitted = true} 
+        type="submit" 
+        name="rating" 
+        value={selected} 
+        class='submit {submitted ? 'submitted' : ''}'>Submit</button>
+</form>
+
+<style>
+
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: var(--s-300);
+    }
+
+    .ratings {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        border: none;
+    }
+
+    label {
+        display: block;
+        position: relative;
+    }
+    
+    input {
+        z-index: -999;
+    }
+
+    label input, label span {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .ratings label {
+        min-width: var(--size-42);
+        min-height: var(--size-42);
+        border-radius: 50%;
+        border: none;
+        background-color: var(--surface-3);
+        color: var(--onsurface-3);
+        font-family: 'Overpass', sans-serif;
+        font-size: var(--font-xs);
+        font-weight: 700;
+        line-height: var(--line-height-lg);
+        cursor: pointer;
+        vertical-align: bottom;
+        /* animation: blurIn 5s ease forwards; */
+    }
+
+    label.selected, label.selected:focus, label:has(input:checked) {
+        background-color: var(--secondary);
+        color: var(--onsecondary);
+        outline: none;
+    }
+
+    label:focus, label:focus-within, label:hover {
+        background-color: var(--primary);
+        color: var(--onprimary);
+        outline: none;
+    }
+
+    .submit {
+        background-color: var(--primary);
+        color: var(--onprimary);
+        border: none;
+        padding: var(--s-200) 0;
+        text-transform: uppercase;
+        font-family: 'Overpass', sans-serif;
+        font-size: var(--font-xs);
+        font-weight: 700;
+        line-height: var(--line-height-lg);
+        cursor: pointer;
+        border-radius: calc(22.5 / 16 * 1rem);
+        width: 100%;
+        vertical-align: center;
+    }
+
+    .submit.submitted {
+        background-color: var(--secondary);
+        color: var(--onsecondary);
+    }
+
+    @media (min-width: 48rem) {
+        .ratings label {
+            min-width: var(--size-51);
+            min-height: var(--size-51);
+            font-size: var(--font-md);
+        }
+
+        form {
+            gap: var(--s-400);
+        }
+
+        .submit {
+            padding: var(--s-100) 0;
+        }
+    }
+
+</style>
